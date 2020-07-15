8e320eb757ddcc541ba3388a415ee5daea70d2ee
DontAtTheNex <67944232+DontAtTheNex@users.noreply.github.com>
2020-07-15 12:56:04 -0500

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,4 +1,3 @@
-'use strict'
 
 /*
 |--------------------------------------------------------------------------
@@ -8,37 +7,38 @@
 | Here we define the configuration for redis server. A single application
 | can make use of multiple redis connections using the redis provider.
 |
-*/
+/
 
 const Env = use('Env')
 
 module.exports = {
-  /*
+  /
   |--------------------------------------------------------------------------
   | connection
   |--------------------------------------------------------------------------
   |
   | Redis connection to be used by default.
   |
-  */
-  connection: Env.get('REDIS_CONNECTION', 'https://leaguegg.tk'),
+  /
+  connection: Env.get('REDIS_CONNECTION', 'local'),
 
-  /*
+  /
   |--------------------------------------------------------------------------
   | local connection config
   |--------------------------------------------------------------------------
   |
   | Configuration for a named connection.
   |
-  */
-  server: {
-    host: Env.get('REDIS_HOST', 'https://leaguegg.tk/'),
+  /
+  local: {
+    host: Env.get('REDIS_HOST', '127.0.0.1'),
+    port: Env.get('REDIS_PORT', 6379),
     password: Env.get('REDIS_PASSWORD', null),
     db: 0,
     keyPrefix: ''
   },
 
-  /*
+  /
   |--------------------------------------------------------------------------
   | cluster config
   |--------------------------------------------------------------------------
